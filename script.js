4679b83fb5683726ee605ad63a7ca3dc14385847
wicker95 <wicker95@gmail.com>
2017-03-31 15:00:00 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+(function() {
+
+  var app = angular.module("githubViewer", []);
+
+  var mainController = function($scope, $http) {
+
+    var onUserComplete = function(response) {
+      $scope.user = response.data;
+      $http.get($scope.user.repos_url)
+        .then(onRepos, onError);
+    };
+
+    var onRepos = function(response){
+      $scope.repos = response.data;
+    }
+
+    var onError = function(reason) {
+      $scope.error = "Could not fetch the data";
+      console.log(reason);
+    };
+
+    $scope.search = function(username){
+      $http.get("https://api.github.com/users/" + username)
+        .then(onUserComplete, onError);      
+    };
+
+    $scope.message = "Github Repo Viewer";
+    $scope.username = "";
+    $scope.repoSortOrder = "-stargazers_count";
+  };
+
+  app.controller("mainController", ["$scope", "$http", mainController]);
+
+}());
